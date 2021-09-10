82fe7780d0f3e8a41a03267b40cbee5a98fd2951
Bruno A. Rolim <brolim@gmail.com>
2021-09-10 12:30:55 -0300

    refact(attack-release): using methods instead of arrays

--- main.rb
+++ main.rb
@@ -25,6 +25,8 @@ class Beats
     ]
   }
 
+  SAMPLE_RATE = 48000
+
   def play music_key_or_encoded_notes
     print 'generating music wave file... '
     encoded_notes = MUSICS[music_key_or_encoded_notes] || music_key_or_encoded_notes
@@ -33,51 +35,36 @@ class Beats
     system 'ffplay -showmode 1 -f f32le -ar 48000 output.bin'
   end
 
-  def generate_wave_file encoded_notes:, volume: 0.2
+  def generate_wave_file encoded_notes:, volume: 0.2, output: 'output.bin'
     binary_wave = encoded_notes.map do |encoded_note|
       generate_binary_sound(
         encoded_note: encoded_note,
-        duration: 0.5,
+        duration: 0.3,
         volume: volume
       )
     end
 
-    File.open('output.bin', 'wb') do |file|
-      binary_wave.flatten.each do |packed_sample|
-        file.write(packed_sample)
-      end
+    File.open(output, 'wb') do |file|
+      binary_wave.flatten.each { |bin_sample| file.write(bin_sample) }
     end
   end
 
-  def generate_binary_sound encoded_note:, duration:, sample_rate: 48000, volume: 1
-    samples_to_generate = (duration * sample_rate).to_i
-    return samples_to_generate.times.map { 0 } if (encoded_note == '_')
+  def generate_binary_sound encoded_note:, duration:, volume: 1
+    samples_to_generate = (duration * SAMPLE_RATE).ceil
+    samples = (0 .. samples_to_generate - 1).to_a
 
-    step = frequency_for_note(encoded_note) * 2 * Math::PI / sample_rate
+    return samples.fill(0) if (encoded_note == '_')
 
-    attack_step = 0.001
-    attack_nsamples = (1.0 / attack_step).to_i
-    attack = (0 .. attack_nsamples - 1).map { |i| i * attack_step }
-
-    release = attack.reverse
-
-    raw_samples = (0 .. samples_to_generate - 1).map do |i|
-      if encoded_note == '_'
-        0.0
-      else
-        vol =
-          if i < attack.size
-            volume * attack[i]
-          elsif samples_to_generate - i < release.size
-            volume * release[samples_to_generate - i]
-          else
-            volume
-          end
-        vol * Math.sin(i * step)
-      end
+    step = frequency_for_note(encoded_note) * 2 * Math::PI / SAMPLE_RATE
+    samples.fill do |i|
+      sample =
+        if encoded_note == '_'
+          0.0
+        else
+          volume * attack(i, duration) * release(i, duration) * Math.sin(i * step)
+        end
+      [sample].pack('e') # float32bitsLE
     end
-
-    raw_samples.map { |sample| [sample].pack('e') }
   end
 
   def frequency_for_note encoded_note
@@ -85,7 +72,21 @@ class Beats
     semitone_number = SEMITONES_BY_LETTER[letter] + 12 * octave.to_i
     440 * (2 ** (1.0 / 12.0)) ** semitone_number
   end
+
+  def attack i, total_duration
+    n_samples_attack = 0.05 * SAMPLE_RATE * total_duration
+    attack_step = 1.0 / n_samples_attack
+    [1.0, i * attack_step].min
+  end
+
+  def release i, total_duration
+    n_samples = SAMPLE_RATE * total_duration
+    n_samples_release = 0.05 * n_samples
+    release_step = 1.0 / n_samples_release
+    [1.0, release_step * (n_samples - i)].min
+  end
 end
 
 beats = Beats.new
 beats.play(:brilha_brilha_estrelinha)
+# beats.play(%w[C.0 C.0 C.0 C.0])
