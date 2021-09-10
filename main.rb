6c485519915fc68fd4dcecd7c280e9a572a2d27c
Bruno A. Rolim <brolim@gmail.com>
2021-09-10 09:05:56 -0300

    feat(attack-and-release): each note now has an attack when started and a release when ended

--- main.rb
+++ main.rb
@@ -25,37 +25,59 @@ class Beats
     ]
   }
 
-  def play music_key
+  def play music_key_or_encoded_notes
     print 'generating music wave file... '
-    binary_wave = MUSICS[music_key].map do |encoded_note|
-      generate_sound(
+    encoded_notes = MUSICS[music_key_or_encoded_notes] || music_key_or_encoded_notes
+    generate_wave_file(encoded_notes: encoded_notes)
+    puts 'OK'
+    system 'ffplay -showmode 1 -f f32le -ar 48000 output.bin'
+  end
+
+  def generate_wave_file encoded_notes:, volume: 0.2
+    binary_wave = encoded_notes.map do |encoded_note|
+      generate_binary_sound(
         encoded_note: encoded_note,
         duration: 0.5,
-        volume: 0.20
+        volume: volume
       )
     end
 
     File.open('output.bin', 'wb') do |file|
-      binary_wave
-        .flatten
-        .each { |packed_sample| file.write(packed_sample) }
+      binary_wave.flatten.each do |packed_sample|
+        file.write(packed_sample)
+      end
     end
-    puts 'OK'
-
-    puts 'playing on ffplay... '
-    system 'ffplay -showmode 1 -f f32le -ar 48000 output.bin'
-    puts 'done'
   end
 
-  def generate_sound encoded_note:, duration:, sample_rate: 48000, volume: 1
+  def generate_binary_sound encoded_note:, duration:, sample_rate: 48000, volume: 1
     samples_to_generate = (duration * sample_rate).to_i
     return samples_to_generate.times.map { 0 } if (encoded_note == '_')
 
     step = frequency_for_note(encoded_note) * 2 * Math::PI / sample_rate
-    (0 .. samples_to_generate).map do |i|
-      sample = encoded_note == '_' ? 0.0 : volume * Math.sin(i * step)
-      [sample].pack('e')
+
+    attack_step = 0.001
+    attack_nsamples = (1.0 / attack_step).to_i
+    attack = (0 .. attack_nsamples - 1).map { |i| i * attack_step }
+
+    release = attack.reverse
+
+    raw_samples = (0 .. samples_to_generate - 1).map do |i|
+      if encoded_note == '_'
+        0.0
+      else
+        vol =
+          if i < attack.size
+            volume * attack[i]
+          elsif samples_to_generate - i < release.size
+            volume * release[samples_to_generate - i]
+          else
+            volume
+          end
+        vol * Math.sin(i * step)
+      end
     end
+
+    raw_samples.map { |sample| [sample].pack('e') }
   end
 
   def frequency_for_note encoded_note
